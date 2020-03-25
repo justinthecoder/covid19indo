80276be82e7b97375e6f06ce15e592433b5373f1
Giovanka Bisano Putra <gbisano@gmail.com>
2020-03-25 07:31:42 +0700

    Remove List modifier to get data from API

--- app/src/main/java/com/covidindo/network/RestApiService.kt
+++ app/src/main/java/com/covidindo/network/RestApiService.kt
@@ -8,6 +8,6 @@ import retrofit2.http.GET
 interface RestApiService {
 
     @GET("indonesia")
-    fun getCovidDataInIndonesia(): Call<List<CovidModel>>
+    fun getCovidDataInIndonesia(): Call<CovidModel>
 
 }
