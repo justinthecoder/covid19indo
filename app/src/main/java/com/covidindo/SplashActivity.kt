6c61075ce9810534621654fcc12b695dc8d28a1d
Giovanka Bisano Putra <gbisano@gmail.com>
2020-03-25 07:48:22 +0700

    Add refresh listener

--- app/src/main/java/com/covidindo/SplashActivity.kt
+++ app/src/main/java/com/covidindo/SplashActivity.kt
@@ -1,19 +1,22 @@
 package com.covidindo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIMEOUT:Long = 2000
+    companion object {
+        const val SPLASH_TIMEOUT: Long = 1000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, SPLASH_TIMEOUT)
     }
